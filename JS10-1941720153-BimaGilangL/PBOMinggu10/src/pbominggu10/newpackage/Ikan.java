26e36980d34b39e9a62834480f507d3034b7d506
BimaGilang <gilangruwet31@gmail.com>
2020-11-12 01:30:16 +0700

    JS10-1941720153-BimaGilangL

---
+++ JS10-1941720153-BimaGilangL/PBOMinggu10/src/pbominggu10/newpackage/Ikan.java
@@ -0,0 +1,10 @@
+
+package pbominggu10.newpackage;
+/**
+ * BimaGilangL
+ */
+public class Ikan extends Hewan{
+    public void bergerak(){
+        System.out.println("Berenang dengan SIRIP,\"Wushh.. Wushh..\"");
+    }
+}
