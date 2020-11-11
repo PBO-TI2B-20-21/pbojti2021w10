3db75724819ef0937c25ae3a517d00793b976f69
zidanshabira <zidanshabira@gmail.com>
2020-11-11 20:30:21 +0700

    JS10_1941720144_Zidan Shabira As Sidiq

---
+++ PBOMinggu10/src/main/java/abstractclass/Hewan.java
@@ -0,0 +1,18 @@
+package abstractclass;
+/**
+ *
+ * @author Zidan
+ */
+public abstract class Hewan {
+    private int umur;
+    
+    protected Hewan(){
+        this.umur=0;
+    }
+    
+    public void bertambahUmur(){
+        this.umur +=1;
+    }
+    
+    public abstract void bergerak();
+}
---
+++ PBOMinggu10/src/main/java/abstractclass/Kucing.java
@@ -0,0 +1,13 @@
+package abstractclass;
+/**
+ *
+ * @author Zidan
+ */
+public class Kucing extends Hewan{
+
+    @Override
+    public void bergerak() {
+        System.out.println("Berjalan dengan KAKI, \"Tap..tap..\"");
+    }
+    
+}
