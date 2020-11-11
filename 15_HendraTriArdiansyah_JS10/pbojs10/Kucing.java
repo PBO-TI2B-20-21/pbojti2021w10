bcdb8c3eb283922c1d94445bc5da0c8bc587a79e
Hendra1177 <tonosupeno12@gmail.com>
2020-11-11 17:46:49 +0700

    15_HendraTriArdiansyah_JS10

---
+++ 15_HendraTriArdiansyah_JS10/pbojs10/Kucing.java
@@ -0,0 +1,9 @@
+package pbojs10;
+public class Kucing extends Hewan {
+
+    @Override
+    public void bergerak() {
+        System.out.println("Berjalan dengan Kaki, \"tap...tap..\"");
+    }
+    
+}
