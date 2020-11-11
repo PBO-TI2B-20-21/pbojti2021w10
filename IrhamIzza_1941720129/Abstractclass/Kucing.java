f2221bc1642fa1619b5ecf37bdcd44311cd1c3f3
IrhamIzza <60720590+IrhamIzza@users.noreply.github.com>
2020-11-11 21:05:30 +0700

    IrhamIzza_1941720129

---
+++ IrhamIzza_1941720129/Abstractclass/Kucing.java
@@ -0,0 +1,8 @@
+package Abstractclass;
+public class Kucing extends Hewan{
+    //Override
+    public void bergerak(){
+        System.out.println("Berjalan Dengan KAKI,\"Tap..tap..\"");
+    }
+    
+}
