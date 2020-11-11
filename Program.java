c7acb92bb0c4d424f0cc4bb26dba100f4fd9dd2b
ardliaqdama09 <71015465+ardliaqdama09@users.noreply.github.com>
2020-11-11 20:08:57 +0700

    18_M.ARDLIAQDAMA_JS10_1941729124

---
+++ Program.java
@@ -0,0 +1,16 @@
+package pbominggu10.abstractclass;
+public class Program {
+    public static void main(String[] args) {
+        Kucing kucingKampung = new Kucing();
+        Ikan lumbalumba = new Ikan();
+        
+        Orang ani =new Orang("Ani");
+        Orang budi = new Orang("Budi");
+        
+        ani.peliharaanHewan(kucingKampung);
+        budi.peliharaanHewan(lumbalumba);
+        
+        ani.ajakPeliharaanJalanJalan();
+        budi.ajakPeliharaanJalanJalan();
+    }
+}
